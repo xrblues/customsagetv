9b053e98465606fa9512fc8f0375f68af04722e5
sean.stuckless <sean.stuckless@00b9ea95-9f52-0410-b88f-4bd5fd4142f9>
2008-09-29 16:44:32 +0000

    Initial import.

---
+++ trunk/ExternalCommandTunerPlugin/ExtTunerTest.c
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2008 - Sean Stuckless
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "ExtTunerPlugin.h"
+
+int main (int argc, char **argv) {
+	printf("Loading Remotes....\n");
+	remote *rems = LoadRemotes("testing 123");
+	
+	printf("Dumping Remotes...\n");
+	DumpRemotes(rems);
+	char *cmd = "215";
+	printf("Playing Command %s ...\n", cmd);
+	PlayCommand(0, rems, "231", 1);
+	printf("Freeing Remotes....\n");
+	FreeRemotes(&rems);
+	
+	printf("Done.\n");
+	
+	return 0;
+}
