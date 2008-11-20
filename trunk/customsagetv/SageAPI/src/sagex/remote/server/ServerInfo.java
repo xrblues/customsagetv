df16424b285bdf349df02447f9fc30e2f01af17d
sean.stuckless <sean.stuckless@00b9ea95-9f52-0410-b88f-4bd5fd4142f9>
2008-11-20 12:45:08 +0000

    added support for jetty added back support for java over http

--- trunk/customsagetv/SageAPI/src/sagex/remote/server/ServerInfo.java
+++ trunk/customsagetv/SageAPI/src/sagex/remote/server/ServerInfo.java
@@ -6,6 +6,7 @@ public class ServerInfo implements Serializable {
 	private static final long serialVersionUID = 3L;
 	public ServerInfo() {
 	}
+	public String url;
 	public String host;
 	public int port;
 }
