58c0113ce8cb144aed9e005c23f3eab5da1e4d10
sean.stuckless <sean.stuckless@00b9ea95-9f52-0410-b88f-4bd5fd4142f9>
2009-03-06 02:28:34 +0000

    Changed the build script to automatically increment the version in the java file.

--- trunk/customsagetv/BatchMetadataTools/src/org/jdna/metadataupdater/Version.java
+++ trunk/customsagetv/BatchMetadataTools/src/org/jdna/metadataupdater/Version.java
@@ -1,5 +1,7 @@
-package org.jdna.metadataupdater;
 
-public class Version {
-    public static final String VERSION = "1.22";
-}
+			package org.jdna.metadataupdater;
+
+			public class Version {
+			    public static final String VERSION = "1.23";
+			}
+		
