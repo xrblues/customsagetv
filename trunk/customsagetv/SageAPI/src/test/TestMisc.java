141f80dd7311c621896057e8fe49e9cdead34ac0
sean.stuckless@gmail.com <sean.stuckless@gmail.com@00b9ea95-9f52-0410-b88f-4bd5fd4142f9>
2012-10-29 22:59:09 +0000

    Added Range header support for sagex/media/mediafile handler

--- trunk/customsagetv/SageAPI/src/test/TestMisc.java
+++ trunk/customsagetv/SageAPI/src/test/TestMisc.java
@@ -7,23 +7,6 @@ public class TestMisc {
 	public static void main(String args[]) {
 		System.out.println(Global.GetServerAddress());
 
-		Object ob [] = MediaFileAPI.GetMediaFiles();
-		for (Object o : ob) {
-			System.out.println("MFNAME: " + MediaFileAPI.GetMediaTitle(o));
-		}
-		MediaFileAPI.GetMediaFiles();
-		
-		ob = MediaFileAPI.GetMediaFiles();
-		for (int i=1;i<10;i++) {
-			System.out.println("GetName: " + MediaFileAPI.GetMediaTitle(ob[i]));
-			try {
-				Thread.sleep(6000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
 		Global.IsClient();
 		Global.IsServerUI();
 		
